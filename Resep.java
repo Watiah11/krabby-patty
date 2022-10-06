5725e1be6c02359c7740c27a54ceb07ec95ce6b0
Watiah11 <watiiahwati111@gmail.com>
2022-10-06 11:28:51 +0700

    APAAN TUH

---
+++ Resep.java
@@ -0,0 +1,11 @@
+public class Resep {
+
+  public static void main(String[] args) {
+    System.out.println("Resep krabby patty");
+    System.out.println("____");
+    System.out.println("1. Bawang");
+    System.out.println("ketimun");
+  }  
+ 
+    
+}
